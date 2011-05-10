2dfcc0bdf11121bc7b5ba8aa814e90b03bf961f6
Martin Holzhauer <martin@holzhauer.eu>
2011-05-10 23:59:23 +0200

    creating all the basefiles

---
+++ README.rst
@@ -0,0 +1,27 @@
+geoHelper
+=========
+
+geoHelper will be a set of php classes to help in the daili work with geodata
+
+
+Goals
+-----
+My goals for the geoHelper is to help me with my geoProblems
+So there will be components for the following usecases:
+ - Common GIS Geometry Objects
+ - Distance calculation (in PHP and creating MySQL querys)
+
+Features that will follow bit later:
+ - GML/KML Parser/Converter
+ - GeoJSON Parser/Converter
+
+Cool to hae componetns would be
+ - converting coordinates between differnet reference systems
+ - WKT parser
+ - PROJ.4 Parser
+
+Requirements
+------------
+I will try to keep the requirements as minimal as possible
+but my dev and productive envoyermetns are PHP 5.3 so this
+is the Version i will support in the early stage
---
+++ src/geometry/Geometry.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ */
+
+namespace de\prolope\geoHelper\geometry;
+
+
+class Geometry {
+//simple
+	public function dimension() {}
+	public function coordinateDimension() {}
+	public function spatialDimension() {}
+	public function geometryType() {}
+	public function SRID() {}
+	public function envelope() {}
+	public function asText() {}
+	public function asBinary () {}
+	public function isEmpty () {}
+	public function isSimple() {}
+	public function is3D() {}
+	public function isMeasured() {}
+	public function boundary () {}
+//query
+	public function equals(Geometry $another ) {}
+	public function disjoint(Geometry $another ) {}
+	public function intersects(Geometry $another ) {}
+	public function touches(Geometry $another ) {}
+	public function crosses(Geometry $another ) {}
+	public function within(Geometry $another ) {}
+	public function contains(Geometry $another ) {}
+	public function overlaps(Geometry $another ) {}
+	public function relate(Geometry $another , $strMatrix) {}
+	public function locateAlong($mValue) {}
+	public function locateBetween($mStart, $mEnd) {}
+//analysis
+	public function distance(Geometry $another ) {}
+	public function buffer($distance) {}
+	public function convexHull() {}
+	public function intersection(Geometry $another ) {}
+	public function union(Geometry $another ) {}
+	public function difference(Geometry $another ) {}
+	public function symDifference(Geometry $another ) {}
+
+}
