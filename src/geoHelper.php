317776a1117fb4f14e6f005f4c389ad765734e45
Martin Holzhauer <martin@holzhauer.eu>
2011-05-11 13:36:40 +0200

    [added] added main geoHelper (because of a proper indexfile in doxygen HTML output)

---
+++ src/geoHelper.php
@@ -0,0 +1,38 @@
+<?php
+/*! \mainpage geoHelper
+ *
+ * \section intro_sec Introduction
+ *
+ * The geoHelper classes are a basic toolset for working with 
+ * geospatial data in PHP
+ *
+ * \section intro_source Where to get the source?
+ *
+ * The Source of the geoHelper can be found on GitHub
+ *
+ *   https://github.com/woodworker/geoHelper
+ *
+ * \section intro_license License
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ */
+
+namespace de\prolope\geoHelper;
+
